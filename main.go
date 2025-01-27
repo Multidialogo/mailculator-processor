c9a99c039fc8dbfdcb11249cae4c5a3a9d3c82a0
zio-mitch <michelecarino84@gmail.com>
2025-01-27 19:10:36 +0100

    FIX: stuff

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"github.com/fsnotify/fsnotify"
+	"os"
 	"path/filepath"
 	"sync"
 	"log"
+	"time"
 
 	"mailculator-processor/internal/config"
 	"mailculator-processor/internal/service"
@@ -15,54 +15,11 @@ import (
 var mutex sync.Mutex
 
 func main() {
+	// Retrieve paths from the configuration
 	registry := config.GetRegistry()
 	basePath := registry.Get("APP_DATA_PATH")
-	draftPath := filepath.Join(basePath, registry.Get("DRAFT_OUTPUT_PATH"))
-	sentPath := filepath.Join(basePath, registry.Get("SENT_OUTPUT_PATH"))
-	failurePath := filepath.Join(basePath, registry.Get("FAILURE_OUTPUT_PATH"))
+	outboxPath := filepath.Join(basePath, registry.Get("OUTBOX_PATH"))
+	sentPath := filepath.Join(basePath, registry.Get("SENT_PATH"))
+	failurePath := filepath.Join(basePath, registry.Get("FAILURE_PATH"))
 
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		panic(fmt.Sprintf("EMERGENCY: %v", err))
-	}
-	defer watcher.Close()
-
-	err = watcher.Add(draftPath)
-	if err != nil {
-		panic(fmt.Sprintf("EMERGENCY: %v", err))
-	}
-
-	for {
-		select {
-		case event := <-watcher.Events:
-			if event.Op&fsnotify.Create == fsnotify.Create {
-				// Lock the file processing to prevent concurrency issues
-				mutex.Lock()
-
-				// Proceed with file processing
-				var destinationPath string
-				err := service.SendEMLFile(event.Name)
-				if err != nil {
-					log.Printf("CRITICAL: %v\n", err)
-					destinationPath = failurePath
-				} else {
-					log.Printf("INFO: %s sent\n", event.Name)
-					destinationPath = sentPath
-				}
-
-				// Move the file and check for errors
-				err = utils.MoveFile(event.Name, destinationPath)
-				if err != nil {
-					// In case of error moving, release the lock and continue
-					mutex.Unlock()
-					panic(fmt.Sprintf("CRITICAL: %v", err))
-				}
-
-				// Unlock the file processing
-				mutex.Unlock()
-			}
-		case err := <-watcher.Errors:
-			panic(fmt.Sprintf("CRITICAL: %v", err))
-		}
-	}
 }

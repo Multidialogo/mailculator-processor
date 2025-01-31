45b57f2fb1646aa0e70eb035e8d1e2422c537745
zio-mitch <michelecarino84@gmail.com>
2025-01-31 15:37:29 +0100

    fixed main go

--- main.go
+++ main.go
@@ -62,6 +62,8 @@ func init() {
 }
 
 func main() {
+	var cycles int = 0
+
 	log.Printf(
 		"\033[36mDEBUG: config -> outbox: %s, sleep time: %d s, old file: %d s, old directory: %d s\033[0m",
 		outboxBasePath, int(sleepTime.Seconds()), int(lastModTime.Seconds()), int(considerEmptyAfterTime.Seconds()),
@@ -72,70 +74,75 @@ func main() {
 		// Record memory and CPU usage at the start
 		printStats()
 		startTime := time.Now()
-
-		// Get the current time and define the lastModTimeThreshold (15 seconds ago)
 		currentTime := time.Now()
-		lastModTimeThreshold := currentTime.Add(lastModTime)
-		considerEmptyAfterTimeThreshold := currentTime.Add(considerEmptyAfterTime)
-		log.Printf("\033[34mINFO: Listing files in: %s, older than %ds\033[0m", outboxBasePath, int(lastModTime.Seconds()))
 
-		// Get list of files to process
-		files, err := utils.ListFiles(outboxBasePath, lastModTimeThreshold)
-		if err != nil {
-			log.Fatalf("Error listing files: %v", err)
+		cycles++
+
+		if cycles == 10 {
+			considerEmptyAfterTimeThreshold := currentTime.Add(considerEmptyAfterTime)
+			// Cleaning up orphans directories from outbox
+			log.Printf("\033[34mINFO: Cleanup outbox %s, older than %ds\033[0m", outboxBasePath, int(considerEmptyAfterTime.Seconds()))
+			err := utils.RemoveEmptyDirs(outboxBasePath, considerEmptyAfterTimeThreshold)
+			if err != nil {
+				log.Printf("\033[31mCRITICAL: Failed to cleanup outbox: %v\033[0m", err)
+			}
+
+			// Sleep for a defined time before processing again
+			log.Printf("\033[34mINFO: Sleeping for %v before recalling the process\033[0m", sleepTime)
+			time.Sleep(sleepTime)
+
+			cycles = 0
+		} else {
+			// Get the current time and define the lastModTimeThreshold (15 seconds ago)
+			lastModTimeThreshold := currentTime.Add(lastModTime)
+			log.Printf("\033[34mINFO: Listing files in: %s, older than %ds\033[0m", outboxBasePath, int(lastModTime.Seconds()))
+
+			// Get list of files to process
+			files, err := utils.ListFiles(outboxBasePath, lastModTimeThreshold)
+			if err != nil {
+				log.Fatalf("Error listing files: %v", err)
+			}
+			log.Printf("\033[34mINFO: Found: %d message files to process\033[0m", len(files))
+
+			// Process each file by calling SendEMLFile in parallel
+			var wg sync.WaitGroup
+
+			for _, file := range files {
+				wg.Add(1) // Increment the WaitGroup counter for each file
+
+				go func(outboxFilePath string) {
+					defer wg.Done() // Decrement the counter when the goroutine completes
+
+					outboxRelativePath := strings.Replace(outboxFilePath, outboxBasePath, "", 1)
+
+					log.Printf("\033[34mINFO: Processing: %s\033[0m", outboxRelativePath)
+
+					// Call the service.SendEMLFile function for each outboxFilePath
+					err, result := service.SendRawEmail(outboxFilePath, rawEmailClient)
+					var destPath string = filepath.Join(basePath, strings.Replace(outboxRelativePath, "/outbox", "", -1))
+					if err != nil {
+						log.Printf("\033[31mCRITICAL: Error processing outboxFilePath %s: %v\033[0m", outboxFilePath, err)
+						destPath = strings.Replace(destPath, "/queues", "/failure/queues", -1)
+					} else {
+						log.Printf("\033[34mINFO: Successfully processed outboxFilePath: %s, result: %v\033[0m", outboxFilePath, result)
+						destPath = strings.Replace(destPath, "/queues", "/sent/queues", -1)
+					}
+
+					log.Printf("\033[34mINFO: Moving file from %s to %s\033[0m", outboxFilePath, destPath)
+					err = utils.MoveFile(outboxFilePath, destPath)
+					if err != nil {
+						log.Printf("\033[31mCRITICAL: Failed to move file from %s to %s: %v\033[0m", outboxFilePath, destPath, err)
+					}
+				}(file) // Pass the file to the goroutine
+			}
+
+			wg.Wait() // Wait for all goroutines to finish
 		}
-		log.Printf("\033[34mINFO: Found: %d message files to process\033[0m", len(files))
-
-		// Process each file by calling SendEMLFile in parallel
-		var wg sync.WaitGroup
-
-		for _, file := range files {
-			wg.Add(1) // Increment the WaitGroup counter for each file
-
-			go func(outboxFilePath string) {
-				defer wg.Done() // Decrement the counter when the goroutine completes
-
-				outboxRelativePath := strings.Replace(outboxFilePath, outboxBasePath, "", 1)
-
-				log.Printf("\033[34mINFO: Processing: %s\033[0m", outboxRelativePath)
-
-				// Call the service.SendEMLFile function for each outboxFilePath
-				err, result := service.SendRawEmail(outboxFilePath, rawEmailClient)
-				var destPath string = filepath.Join(basePath, strings.Replace(outboxRelativePath, "/outbox", "", -1))
-				if err != nil {
-					log.Printf("\033[31mCRITICAL: Error processing outboxFilePath %s: %v\033[0m", outboxFilePath, err)
-					destPath = strings.Replace(destPath, "/queues", "/failure/queues", -1)
-				} else {
-					log.Printf("\033[34mINFO: Successfully processed outboxFilePath: %s, result: %v\033[0m", outboxFilePath, result)
-					destPath = strings.Replace(destPath, "/queues", "/sent/queues", -1)
-				}
-
-				log.Printf("\033[34mINFO: Moving file from %s to %s\033[0m", outboxFilePath, destPath)
-				err = utils.MoveFile(outboxFilePath, destPath)
-				if err != nil {
-					log.Printf("\033[31mCRITICAL: Failed to move file from %s to %s: %v\033[0m", outboxFilePath, destPath, err)
-				}
-			}(file) // Pass the file to the goroutine
-		}
-
-		wg.Wait() // Wait for all goroutines to finish
 
 		log.Printf("\u001B[36mDEBUG: Elapsed time: %.2f seconds\n\033[0m", time.Since(startTime).Seconds())
 
-		// Cleaning up orphans directories from outbox
-		log.Printf("\033[34mINFO: Cleanup outbox %s, older than %ds\033[0m", outboxBasePath, int(considerEmptyAfterTime.Seconds()))
-		err = utils.RemoveEmptyDirs(outboxBasePath, considerEmptyAfterTimeThreshold)
-		if err != nil {
-			log.Printf("\033[31mCRITICAL: Failed to cleanup outbox: %v\033[0m", err)
-		}
-
-		log.Printf("\u001B[36mDEBUG: Elapsed time with cleanup: %.2f seconds\n\033[0m", time.Since(startTime).Seconds())
 		// Record memory and CPU usage at the end
 		printStats()
-
-		// Sleep for a defined time before processing again
-		log.Printf("\033[34mINFO: Sleeping for %v before recalling the process\033[0m", sleepTime)
-		time.Sleep(sleepTime)
 	}
 
 }

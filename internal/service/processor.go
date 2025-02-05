62422e96f874de74a1d19c0d7c87dabac0cee548
zio-mitch <michelecarino84@gmail.com>
2025-02-05 15:55:19 +0100

    Manage concurrency on autoscaling

--- internal/service/processor.go
+++ internal/service/processor.go
@@ -3,10 +3,27 @@ package service
 import (
 	"fmt"
 	"os"
+
+	"github.com/gofrs/flock"
 )
 
 // SendRawEmail sends a raw email using the provided RawEmailClient
 func SendRawEmail(filePath string, client RawEmailClient) (error, *RawEmailOutput) {
+	// Create a flock instance for the lock file
+	lock := flock.New(filePath)
+
+	// Try to acquire an exclusive lock on the file
+	locked, err := lock.TryLock()
+	if err != nil {
+		// If an error occurred, print it
+		return fmt.Errorf("Error locking file: %v", err), nil
+	}
+
+	// If the file is locked, skip processing
+	if !locked {
+		return fmt.Errorf("File locked: %v", err), nil
+	}
+
 	// Open the .EML file
 	file, err := os.Open(filePath)
 	if err != nil {
@@ -30,6 +47,8 @@ func SendRawEmail(filePath string, client RawEmailClient) (error, *RawEmailOutpu
 	if err != nil {
 		return fmt.Errorf("failed to send email: %v", err), nil
 	}
+
+	lock.Unlock()
 
 	return nil, result
 }

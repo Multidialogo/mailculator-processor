58f68c4cc7c3594905dd4cc00a054e94d212feb0
zio-mitch <michelecarino84@gmail.com>
2025-02-10 13:24:10 +0100

    Fix add all metrics utils in metrics

--- main.go
+++ main.go
@@ -11,8 +11,6 @@ import (
 
 	"mailculator-processor/internal/config"
 	"mailculator-processor/internal/utils"
-	"github.com/shirou/gopsutil/cpu"
-	"github.com/shirou/gopsutil/mem"
 )
 
 var container *config.Container
@@ -44,7 +42,11 @@ func main() {
 	// Main loop to process files periodically
 	for {
 		// Record memory and CPU usage at the start
-		printStats()
+		err := container.Metrics.CollectMemoryAndCpu()
+		if err != nil {
+			log.Printf("\033[31mCRITICAL: %v\033[0m", err)
+		}
+
 		startTime := time.Now()
 		currentTime := time.Now()
 
@@ -54,7 +56,7 @@ func main() {
 			considerEmptyAfterTimeThreshold := currentTime.Add(considerEmptyAfterTime)
 			// Cleaning up orphans directories from outbox
 			log.Printf("\033[34mINFO: Cleanup outbox %s, older than %ds\033[0m", outboxBasePath, int(considerEmptyAfterTime.Seconds()))
-			err := utils.RemoveEmptyDirs(outboxBasePath, considerEmptyAfterTimeThreshold)
+			err = utils.RemoveEmptyDirs(outboxBasePath, considerEmptyAfterTimeThreshold)
 			if err != nil {
 				log.Printf("\033[31mCRITICAL: Failed to cleanup outbox: %v\033[0m", err)
 			}
@@ -119,38 +121,9 @@ func main() {
 		log.Printf("\u001B[36mDEBUG: Elapsed time: %.2f seconds\n\033[0m", time.Since(startTime).Seconds())
 
 		// Record memory and CPU usage at the end
-		printStats()
+		err = container.Metrics.CollectMemoryAndCpu()
+		if err != nil {
+			log.Printf("\033[31mCRITICAL: %v\033[0m", err)
+		}
 	}
-}
-
-// printStats logs the memory and CPU stats and updates the Prometheus metrics
-func printStats() {
-	// Memory stats using gopsutil/mem
-	v, err := mem.VirtualMemory()
-	if err != nil {
-		log.Printf("\033[31mCRITICAL: Error fetching memory usage: %v\033[0m", err)
-	}
-
-	// Update the Prometheus memory usage gauge
-	container.Metrics.MemoryUsageGauge.WithLabelValues("total").Set(float64(v.Total)) // Total memory in bytes
-	container.Metrics.MemoryUsageGauge.WithLabelValues("used").Set(float64(v.Used))   // Used memory in bytes
-	container.Metrics.MemoryUsageGauge.WithLabelValues("free").Set(float64(v.Free))   // Free memory in bytes
-	container.Metrics.MemoryUsageGauge.WithLabelValues("percent").Set(v.UsedPercent)  // Percent used
-
-	// CPU stats
-	cpus, err := cpu.Percent(0, false)
-	if err != nil {
-		log.Printf("\033[31mCRITICAL: Error fetching CPU usage: %v\033[0m", err)
-	}
-
-	// Update the Prometheus CPU usage gauge
-	if len(cpus) > 0 {
-		container.Metrics.CpuUsageGauge.WithLabelValues("cpu0").Set(cpus[0]) // Assuming a single CPU for simplicity, can be extended for multiple CPUs
-	}
-
-	log.Printf(
-		"\u001B[33mDEBUG: MEMORY: Total = %v MiB, Used = %v MiB, Free = %v MiB, Percent = %v%%\033[0m",
-		v.Total/1024/1024, v.Used/1024/1024, v.Free/1024/1024, v.UsedPercent,
-	)
-	log.Printf("\u001B[33mDEBUG: CPU: Peak Usage = %v%%\033[0m", cpus[0])
 }

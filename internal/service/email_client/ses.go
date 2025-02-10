797ba820d6ec0ac02b808beba86182622d6fbc39
zio-mitch <michelecarino84@gmail.com>
2025-02-10 17:30:55 +0100

    fix dep

--- internal/service/email_client/ses.go
+++ internal/service/email_client/ses.go
@@ -2,7 +2,7 @@ package email_client
 
 import (
 	"context"
-	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ses"
 	"github.com/aws/aws-sdk-go-v2/service/ses/types"
 )

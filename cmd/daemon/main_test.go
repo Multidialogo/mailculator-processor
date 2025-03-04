c1eead8247bb6313583c3725b422a2a31891b341
Emidio Cruciani <cruciani@multidialogo.it>
2025-03-04 17:39:11 +0100

    gracefully close when receive SIGTERM signal

--- cmd/daemon/main_test.go
+++ cmd/daemon/main_test.go
@@ -5,20 +5,40 @@ import (
 	"github.com/stretchr/testify/assert"
 	"mailculator-processor/internal/config"
 	"mailculator-processor/internal/daemon"
+	"os"
+	"syscall"
 	"testing"
 	"time"
 )
 
-func newDaemonRunMockFunc(ctx context.Context) func(context.Context, *config.Container) {
+func newDaemonRunMockFuncWithTimeout() (func(context.Context, *config.Container), context.CancelFunc) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	return func(_ context.Context, container *config.Container) {
 		daemon.Run(ctx, container)
-	}
+	}, cancel
 }
 
-func TestMainFunc(t *testing.T) {
-	ctx, cancelTestCtx := context.WithTimeout(context.Background(), time.Second)
-	defer cancelTestCtx()
+func TestMainWillGracefullyShutdownWhenSigtermSignal(t *testing.T) {
+	// a timeout is needed to ensure function will stop even if sigterm does not work
+	daemonRunMockFunc, cancel := newDaemonRunMockFuncWithTimeout()
+	defer cancel()
 
-	daemonRunFunc = newDaemonRunMockFunc(ctx)
+	daemonRunFunc = daemonRunMockFunc
+	go sendSigtermSignalInOneSecond(t)
+
 	assert.NotPanics(t, main)
+}
+
+func sendSigtermSignalInOneSecond(t *testing.T) {
+	time.Sleep(time.Second)
+
+	p, err := os.FindProcess(os.Getpid())
+	if err != nil {
+		t.Fatal("failed to find process to send signal")
+	}
+
+	err = p.Signal(syscall.SIGTERM)
+	if err != nil {
+		t.Fatal("failed to send SIGTERM")
+	}
 }

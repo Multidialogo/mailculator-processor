8e888f5c34ff36a5e9807572bce6db22f7eb5e76
Matteo Pomi <6242691+NeverWise@users.noreply.github.com>
2025-03-06 10:27:18 +0100

    Changed init-ses.sh

--- local/docker/localstack/init-ses.sh
+++ local/docker/localstack/init-ses.sh
@@ -1,17 +1,3 @@
 #!/bin/sh
 
 awslocal ses verify-email-identity --email-address sender@test.multidialogo.it
-
-awslocal dynamodb create-table \
-    --table-name Outbox \
-    --attribute-definitions \
-       AttributeName=id,AttributeType=S \
-    --key-schema AttributeName=id,KeyType=HASH \
-    --billing-mode PAY_PER_REQUEST
-
-awslocal dynamodb create-table \
-    --table-name OutboxLock \
-    --attribute-definitions \
-       AttributeName=id,AttributeType=S \
-    --key-schema AttributeName=id,KeyType=HASH \
-    --billing-mode PAY_PER_REQUEST

1a3d5694549e96d41f70273582f2b5b8c8746fc0
Emidio Cruciani <cruciani@multidialogo.it>
2025-03-11 13:00:55 +0100

    local and deployment provisioning refactoring

---
+++ local/stop-devenv.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+script_dir=$(dirname "$(realpath -s "$0")")
+
+deployments_dir="$script_dir/../deployments"
+
+docker compose -f "$deployments_dir/compose.yml" --profile local-dev down --remove-orphans -v
--- local/test.sh
+++ local/test.sh
@@ -2,8 +2,12 @@
 
 script_dir=$(dirname $(realpath -s $0))
 
-docker compose -f "$script_dir/docker/compose.yml" exec app go mod tidy
+root_dir="$script_dir/.."
 
-docker compose -f "$script_dir/docker/compose.yml" exec app go test ./...
+deployments_dir="$root_dir/deployments"
 
-chown 1000:1000 -R "$script_dir/.."
+docker compose -f "$deployments_dir/compose.yml" exec local-dev go mod tidy
+
+docker compose -f "$deployments_dir/compose.yml" exec local-dev go test ./...
+
+chown 1000:1000 -R "$root_dir"

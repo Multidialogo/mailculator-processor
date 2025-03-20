93fdf4f7dd1284ed1097eed153a8d9811670c58c
Matteo Pomi <6242691+NeverWise@users.noreply.github.com>
2025-03-20 15:06:35 +0100

    Added loader_test.go, fixed a bug in loader.go, made minor improvements, and updated the dependencies

--- deployments/resources/coverage.sh
+++ deployments/resources/coverage.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
+temp_filename='coverage.out'
+report_dir='.coverage'
+report_filename='report.html'
+report_path="$report_dir/$report_filename"
+
 go mod tidy
 
-go run gotest.tools/gotestsum@latest ./... -coverpkg=./... -coverprofile=coverage.out
+go run gotest.tools/gotestsum@latest ./... -coverpkg=./... -coverprofile=$temp_filename
 
-cov=$(go tool cover -func coverage.out | grep -E "^total" | grep -o -E "[0-9]*\.[0-9]*%$")
+cov=$(go tool cover -func $temp_filename | grep -E "^total" | grep -o -E "[0-9]*\.[0-9]*%$")
 echo "Total coverage: ${cov}"
 
-mkdir -p ./.coverage
+mkdir -p "./$report_dir"
 
-go tool cover -html=coverage.out -o ./.coverage/report.html
-echo "Report exported at .coverage/report.html"
+go tool cover -html=$temp_filename -o "./$report_path"
+echo "Report exported at $report_path"
 
-rm ./coverage.out
+rm "./$temp_filename"

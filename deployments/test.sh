b949f2c5070358316b88038e01059893f8f60575
Emidio Cruciani <cruciani@multidialogo.it>
2025-03-24 15:54:38 +0100

    refactored deployments files after implementation of devcontainer

--- deployments/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-script_dir=$(dirname "$(realpath -s "$0")")
-
-docker compose -f "$script_dir/compose.yml" --profile test-deps up -d --build --force-recreate
-trap "echo " INT
-docker compose -f "$script_dir/compose.yml" run --rm test
-docker compose -f "$script_dir/compose.yml" --profile test-deps down --remove-orphans -v
---
+++ run-tests-local.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+script_dir=$(dirname "$(realpath -s "$0")")
+
+coverage() {
+  temp_path="$script_dir/coverage.out"
+  report_dir="$script_dir/.coverage"
+  report_filename="report.html"
+  report_path="$report_dir/$report_filename"
+
+  export AWS_BASE_ENDPOINT=http://127.0.0.1:4566
+  export AWS_ACCESS_KEY_ID=local
+  export AWS_SECRET_ACCESS_KEY=local
+  export AWS_REGION=eu-west-1
+  export SMTP_HOST=127.0.0.1
+  export SMTP_USER=user
+  export SMTP_PASS=pass
+  export SMTP_PORT=1025
+  export SMTP_FROM=mailer@example.com
+
+  go mod tidy
+  go test ./... -coverpkg=./... -coverprofile=$temp_path
+
+  cov=$(go tool cover -func $temp_path | grep -E "^total" | grep -o -E "[0-9]*\.[0-9]*%$")
+  echo "Total coverage: ${cov}"
+
+  mkdir -p $report_dir
+  go tool cover -html=$temp_path -o $report_path
+  echo "Report exported at $report_path"
+  rm $temp_path
+}
+
+if ! docker compose -f "$script_dir/compose.yml" --profile test-deps up -d --build --force-recreate; then
+  echo "Could not start test dependencies"
+  exit 1
+fi
+
+# sleep to give time to localstack to start database
+sleep 5
+
+# run in subshell to avoid exporting env variables
+(coverage)
+
+docker compose -f "$script_dir/compose.yml" --profile test-deps down --remove-orphans -v

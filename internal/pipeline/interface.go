d9c881d35949e07593d6b7f898ea1d1c4c89e567
Emidio Cruciani <cruciani@multidialogo.it>
2025-11-28 10:54:51 +0000

    TTL retrocompatibility: do not set when TTL not found in attributes

--- internal/pipeline/interface.go
+++ internal/pipeline/interface.go
@@ -7,6 +7,6 @@ import (
 
 type outboxService interface {
 	Query(ctx context.Context, status string, limit int) ([]outbox.Email, error)
-	Update(ctx context.Context, id string, status string, errorReason string, ttl int64) error
-	Ready(ctx context.Context, id string, emlFilePath string, ttl int64) error
+	Update(ctx context.Context, id string, status string, errorReason string, ttl *int64) error
+	Ready(ctx context.Context, id string, emlFilePath string, ttl *int64) error
 }
